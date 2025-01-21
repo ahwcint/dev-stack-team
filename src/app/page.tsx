9a252faa4f956de56a2ba9ee1374d09cecf1f16d
aumer <chanwit.tan@everapp.io>
2025-01-21 22:46:23 +0700

    [major update] setup heroui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 import { useSocket } from '@/hooks/useSocket/useSocket';
-import { useState } from 'react';
+import { Divider, Button, Card, CardBody, CardHeader } from '@heroui/react';
 
 export default function Home() {
-  const check = useState(true);
-  useSocket(check[0]);
+  useSocket(true);
   return (
     <div>
-      <button onClick={() => check[1](true)} className="block">
-        connect
-      </button>
-      <button onClick={() => check[1](false)}>disconnect</button>
+      <Button>TEST</Button>
+      <Card>
+        <CardHeader>Title</CardHeader>
+        <Divider />
+        <CardBody>This is content</CardBody>
+      </Card>
     </div>
   );
 }

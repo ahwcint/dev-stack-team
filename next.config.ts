4fc08f04aad8cbbb97d6adc7e6161aab3f92f5a1
aumer <chanwit.tan@everapp.io>
2025-01-23 04:04:12 +0700

    s
    fix type
    
    eslint
    
    fix: eslint valid ignore
    
    feat: add next ignore path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   env: {
-    SOCKET_LOCAL: process.env.SOCKET_LOCAL,
+    API_URL_PATH: process.env.API_URL_PATH,
   },
 };
 
